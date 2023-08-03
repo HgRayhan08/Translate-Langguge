d52bd3131df34ec2ae47e4dc04445b24787ebbca
Hugo Rayhan Firmansyah <hugo.hgrayhan08@gmail.com>
2023-08-04 00:10:46 +0700

    add leaguage korea

--- lib/data/langage_code.dart
+++ lib/data/langage_code.dart
@@ -2,10 +2,12 @@ List<String> languages = [
   'English',
   'Indonesian',
   'Mandarin',
+  'Korea',
 ];
 
 Map<String, String> languageCodes = {
   'English': 'en',
   'Indonesian': 'id',
   'Mandarin': 'zh-CN',
+  'Korea': 'ko',
 };
